155b21bebd04c9c8d2cf31937c9b22fcb64791e3
Remberto Villar <rembertovillar@gmail.com>
2023-08-03 17:06:32 -0400

    update origins.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,5 @@
-const allowedOrigins = ["https://notes-t5qx.onrender.com"]
+const allowedOrigins = ["https://notes-t5qx.onrender.com",
+    "http://localhost:5173"
+]
 
 module.exports = allowedOrigins
